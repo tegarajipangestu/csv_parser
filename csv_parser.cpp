85e8a525e9e1e8681b669e9a33a9e22fd5f51df5
tegarajipangestu <tegarajipangestu@gmail.com>
2016-11-10 13:27:34 +0700

    Make it unsigned

--- csv_parser.cpp
+++ csv_parser.cpp
@@ -159,7 +159,7 @@ int main(int argc, char *argv[])
     str = read_from_file(file);
     contents = parse_values(str);
 
-    for (int i=0;i<attributes.size();i++) {
+    for (unsigned int i=0;i<attributes.size();i++) {
       ofstream output_file((file_name+"-"+attributes.at(i)+"-"+int_to_string(i)+".csv").c_str());
       for (vector< vector<string> >::iterator vec_it = contents.begin() ; vec_it != contents.end() ; ++vec_it) {
         output_file << (*vec_it).at(i) << endl;
