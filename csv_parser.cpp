3b25494bb0e84ea8e5c1e2aeb38e635964fad411
tegarajipangestu <tegarajipangestu@gmail.com>
2016-11-10 19:46:30 +0700

    Fancy format

--- csv_parser.cpp
+++ csv_parser.cpp
@@ -1,5 +1,11 @@
 /*
-Your code is to read in a single CSV and output a series of files that are run length encoded (https://en.wikipedia.org/wiki/Run-length_encoding), where each output file corresponds to an attribute (column) from the input file. The output files should be named [inputfilename]-[attributename]-[attributenumber].csv and written to the same directory as the input files. The output files will have two values per line. The first value will be the value from the original file, the second value is the number of times that value appears consecutively.  The values should be comma delimited. The number of lines per file will vary with the level of repetition.
+Your code is to read in a single CSV and output a series of files that are run length encoded (https://
+en.wikipedia.org/wiki/Run-length_encoding), where each output file corresponds to an attribute (column)
+from the input file. The output files should be named [inputfilename]-[attributename]-[
+attributenumber].csv and written to the same directory as the input files. The output files will have two
+values per line. The first value will be the value from the original file, the second value is the number
+of times that value appears consecutively.  The values should be comma delimited. The number of lines per
+file will vary with the level of repetition.
 */
 
 #include <fstream>
@@ -110,7 +116,7 @@ vector< vector<string> > parse_values(string line) {
         }
         else if (state == READ_WITHOUT_QUOTES && (line[i] == '\'' || line[i] == '\"')) {
           cout << line[i-1] << " " << line[i] << endl;
-          throw "Malformed CSV. You need to escape character at "+int_to_string(rows.size());
+          throw "Malformed CSV";
         }
         else {
           word += line[i];
