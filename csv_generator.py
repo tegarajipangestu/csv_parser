46ba6ea5f5af92641e1c1cadfdde1081506340de
tegarajipangestu <tegarajipangestu@gmail.com>
2016-11-10 19:23:16 +0700

    Unnecessary changes, just for generating csv

--- csv_generator.py
+++ csv_generator.py
@@ -5,13 +5,8 @@ from datetime import datetime
 
 
 l=Faker('en_GB')
-f=open("test.csv","r")
-k=csv.reader(f)
-
 g=open("1.csv","w")
 w=csv.writer(g)
 w.writerow(('id','name','address','college','company','dob','age'))
-for i in range(20000000):
-
+for i in range(20):
     w.writerow((i+1,"Some name","Some address",random.choice(['psg','sona','amirta','anna university']),random.choice(['CTS','INFY','HTC']),(random.randrange(1950,1995,1),random.randrange(1,13,1),random.randrange(1,32,1)),random.choice(range(0,100))))
-f.close()
